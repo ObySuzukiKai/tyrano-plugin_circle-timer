3440b8a77685b2aad777f7f833212d4e89b786a9
小鳥遊銀 <48020662+TakanashiGin@users.noreply.github.com>
2022-12-17 02:18:04 +0000

    fix

--- develop/setup.sh
+++ develop/setup.sh
@@ -5,7 +5,6 @@ mkdir ./tyranoscript/data/others
 mkdir ./tyranoscript/data/others/plugin
 cp ./index.js ./tyranoscript/
 cp -r ./circle_timer/ ./tyranoscript/data/others/plugin/
-cp ./circle_timer/_SAMPLE.ks ./tyranoscript/data/scenario/
 
 cp ./tyranoscript/data/scenario/first.ks ./tyranoscript/data/scenario/first.origin.ks
 sed -e 's/@jump storage="title.ks"/@jump storage="..\/others\/plugin\/circle_timer\/_SAMPLE.ks"/g' ./tyranoscript/data/scenario/first.origin.ks > ./tyranoscript/data/scenario/first.ks
